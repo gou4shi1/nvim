982714caf9d3ec1a53d6502e640467b4eb8d6c29
Guangqing Chen <hi@goushi.me>
2025-02-24 20:52:04 +0800

    [lualine] remove redundant spaces

--- lua/plugins/appearance/lualine.lua
+++ lua/plugins/appearance/lualine.lua
@@ -1,3 +1,46 @@
+local function root_dir(opts)
+  opts = vim.tbl_extend("force", {
+    cwd = false,
+    subdirectory = true,
+    parent = true,
+    other = true,
+    icon = "󱉭 ",
+    color = function()
+      return { fg = Snacks.util.color("Special") }
+    end,
+  }, opts or {})
+
+  local function get()
+    local cwd = LazyVim.root.cwd()
+    local root = LazyVim.root.get({ normalize = true })
+    local name = vim.fs.basename(root)
+
+    if root == cwd then
+      -- root is cwd
+      return opts.cwd and name
+    elseif root:find(cwd, 1, true) == 1 then
+      -- root is subdirectory of cwd
+      return opts.subdirectory and name
+    elseif cwd:find(root, 1, true) == 1 then
+      -- root is parent directory of cwd
+      return opts.parent and name
+    else
+      -- root and cwd are not related
+      return opts.other and name
+    end
+  end
+
+  return {
+    function()
+      return (opts.icon and opts.icon) .. get()
+    end,
+    cond = function()
+      return type(get()) == "string"
+    end,
+    color = opts.color,
+  }
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
@@ -28,14 +71,17 @@ return {
         lualine_a = { "mode" },
         lualine_b = { "branch" },
         lualine_c = {
-          LazyVim.lualine.root_dir(),
+          root_dir(),
           {
             "filetype",
             icon_only = true,
             separator = "",
             padding = { left = 1, right = 0 }
           },
-          { LazyVim.lualine.pretty_path() },
+          {
+            LazyVim.lualine.pretty_path(),
+            padding = { left = 0, right = 1 },
+          },
           {
             'b:coc_current_function',
             color = {

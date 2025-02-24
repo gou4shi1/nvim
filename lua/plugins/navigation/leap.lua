3b9311a58d273870e229a4c0166cb2bfd65e0d35
Guangqing Chen <hi@goushi.me>
2025-02-25 00:26:11 +0800

    [navigation] add flash, leap to jump to anywhere

---
+++ lua/plugins/navigation/leap.lua
@@ -0,0 +1,17 @@
+return {
+  "ggandor/leap.nvim",
+  keys = {
+    { "s", mode = { "n", "x", "o" }, desc = "Jump to anywhere with 2-chars." },
+  },
+  opts = {
+    equivalence_classes = { ' \t\r\n', '[{', ']}', '\'"`' },
+  },
+  config = function(_, opts)
+    local leap = require("leap")
+    for k, v in pairs(opts) do
+      leap.opts[k] = v
+    end
+    vim.keymap.set('n', 's', '<Plug>(leap-anywhere)')
+    vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap)')
+  end,
+}

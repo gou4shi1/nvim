2ce9d8d4e5ad68d24c343d445cdd3bbee643f291
Guangqing Chen <hi@goushi.me>
2025-02-28 13:15:35 +0800

    [solarized] customize semantic highlights

--- lua/plugins/appearance/solarized.lua
+++ lua/plugins/appearance/solarized.lua
@@ -3,14 +3,22 @@ return {
   optional = true,
   opts = {
     variant = 'autumn',
+    on_highlights = function(colors, color)
+      return {
+        SpellBad = { undercurl = true, strikethrough = false },
+        CursorLineGitBlame = { fg = "#4b5c6d", italic = true },
+        -- semantic
+        Parameter = { link = "Identifier" },
+        CocSemTypeEnumMember = { fg = "#AD7FA8" },
+        CocSemTypeNamespace = { fg = "DarkCyan" },
+        CocSemTypeProperty = { fg = "DarkGrey" },
+      }
+    end,
   },
   config = function(_, opts)
     vim.o.termguicolors = true
     vim.o.background = 'dark'
     require('solarized').setup(opts)
     vim.cmd.colorscheme 'solarized'
-
-    vim.api.nvim_set_hl(0, "SpellBad", { undercurl = true })
-    vim.api.nvim_set_hl(0, "CursorLineGitBlame", { fg = "#4b5c6d", italic = true })
   end,
 }

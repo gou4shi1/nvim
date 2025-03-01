09e17702c67e69e36637bc5f8c117de02761e0db
Guangqing Chen <hi@goushi.me>
2025-03-01 23:15:07 +0800

    [git] add git blame file

--- lua/plugins/git/blame.lua
+++ lua/plugins/git/blame.lua
@@ -1,13 +1,27 @@
 return {
-  "f-person/git-blame.nvim",
-  event = { "VeryLazy" },
-  opts = {
-    message_template = "    <author> • <date> • <summary>",
-    date_format = "%r",
-    virtual_text_column = 20,
-    highlight_group = "CursorLineGitBlame",
-    set_extmark_options = {
-      hl_mode = "combine",
+  {
+    "f-person/git-blame.nvim",
+    event = { "VeryLazy" },
+    opts = {
+      message_template = "    <author> • <date> • <summary>",
+      date_format = "%r",
+      virtual_text_column = 20,
+      highlight_group = "CursorLineGitBlame",
+      set_extmark_options = {
+        hl_mode = "combine",
+      },
+    },
+  },
+
+  {
+    "FabijanZulj/blame.nvim",
+    cmd = "BlameToggle",
+    keys = {
+      { "<Leader>gB", "<cmd>BlameToggle<cr>", desc = "Git Blame File" },
+    },
+    opts = {
+      date_format = "%Y.%m.%d",
+      blame_options = { '-w' },
     },
   },
 }

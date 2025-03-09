b187b43d26c50f75a6b1d6b94e87ee5f8536be7f
Guangqing Chen <hi@goushi.me>
2025-03-09 21:21:41 +0800

    [language] add nvim-lint

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Add support for the LazyFile event
+local Event = require("lazy.core.handler.event")
+Event.mappings.LazyFile = { id = "LazyFile", event = { "BufReadPost", "BufNewFile", "BufWritePre" } }
+Event.mappings["User LazyFile"] = Event.mappings.LazyFile
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {

17b42b1d772e6703b4091142333dcc01112cb32f
Guangqing Chen <hi@goushi.me>
2025-03-25 15:37:00 +0800

    [run] map \\ to exit insert mode in terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 vim.keymap.set("v", "<Leader>y", '"+y', { silent = true, desc = "Yank to system clipboard." })
 vim.keymap.set("n", "<Leader>p", '"+p', { silent = true, desc = "Paste from system clipboard." })
 
+-- terminal
+vim.keymap.set("t", "<localleader><localleader>", "<C-\\><C-n>", { silent = true, desc = "Exit insert mode in terminal." })
+
 -- %% -> local path
 vim.keymap.set(
   'c',

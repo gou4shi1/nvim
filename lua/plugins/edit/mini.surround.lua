8db83108e8eedf3c58121cdc697a56584713a930
Guangqing Chen <hi@goushi.me>
2025-03-28 18:12:49 +0800

    [edit] add mini.surround

---
+++ lua/plugins/edit/mini.surround.lua
@@ -0,0 +1,15 @@
+return {
+  "echasnovski/mini.surround",
+  keys = {
+    { "ys", desc = "Add Surrounding",    mode = { "n", "v" } },
+    { "ds", desc = "Delete Surrounding" },
+    { "cs", desc = "Replace Surrounding" },
+  },
+  opts = {
+    mappings = {
+      add = "ys",
+      delete = "ds",
+      replace = "cs",
+    },
+  },
+}

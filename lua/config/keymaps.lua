9f344fe66fb737a1c3505ee6993e9008cfdc0a3d
Guangqing Chen <hi@goushi.me>
2025-03-29 15:38:33 +0800

    [language] add neogen to generate doc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,22 @@
 vim.keymap.set("v", "<Leader>y", '"+y', { silent = true, desc = "Yank to system clipboard." })
 vim.keymap.set("n", "<Leader>p", '"+p', { silent = true, desc = "Paste from system clipboard." })
 
+-- snippet
+vim.keymap.set({ 'i', 's' }, '<C-j>', function()
+  if vim.snippet.active({ direction = 1 }) then
+    return '<cmd>lua vim.snippet.jump(1)<cr>'
+  else
+    return '<C-j>'
+  end
+end, { expr = true })
+vim.keymap.set({ 'i', 's' }, '<C-k>', function()
+  if vim.snippet.active({ direction = -1 }) then
+    return '<cmd>lua vim.snippet.jump(-1)<cr>'
+  else
+    return '<C-k>'
+  end
+end, { expr = true })
+
 -- terminal
 vim.keymap.set("t", "<localleader><localleader>", "<C-\\><C-n>", { silent = true, desc = "Exit insert mode in terminal." })
 

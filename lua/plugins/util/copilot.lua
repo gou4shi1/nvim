4e5a120520fb9fb2a4222c05e05602c9234b57e5
Guangqing Chen <hi@goushi.me>
2026-03-02 15:25:59 +0800

    [ai] replace codecompanion by sidekick

--- lua/plugins/util/copilot.lua
+++ lua/plugins/util/copilot.lua
@@ -10,22 +10,50 @@ return {
     },
   },
   {
-    "olimorris/codecompanion.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
+    "folke/sidekick.nvim",
     opts = {
-      strategies = {
-        chat = {
-          opts = {
-            completion_provider = "coc",
-          },
+      nes = { enabled = false },
+      cli = {
+        mux = {
+          enabled = true,
+          backend = "zellij",
         },
       },
     },
     keys = {
-      { "<leader>cc", "<cmd>CodeCompanion<cr>", desc = "Trigger CodeCompanion" },
+      {
+        "<leader>cc",
+        function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
+        desc = "Sidekick Toggle Claude",
+      },
+      {
+        "<leader>cd",
+        function() require("sidekick.cli").close() end,
+        desc = "Detach a CLI Session",
+      },
+      {
+        "<leader>ct",
+        function() require("sidekick.cli").send({ msg = "{this}" }) end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>cf",
+        function() require("sidekick.cli").send({ msg = "{file}" }) end,
+        desc = "Send File",
+      },
+      {
+        "<leader>cv",
+        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        "<leader>cp",
+        function() require("sidekick.cli").prompt() end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
     },
   },
 }
